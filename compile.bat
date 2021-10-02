15ebc14d7bc8529f8bef09ccbc2681ca07dc72e2
Sgeo <sgeoster@gmail.com>
2021-10-02 16:38:04 -0400

    Revert "added rofs support for repo-mount"

--- compile.bat
+++
@@ -1,6 +0,0 @@
-:: expects to start in win96-repos folder (this folder)
-node "meta files generator.js" apps
-node "meta files generator.js" malware
-cd ..
-node "win96-repos/fsbuilder.js" win96-repos
-pause
